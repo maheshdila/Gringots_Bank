3da4d879cf729c4787819702c519a93092877ac0
maheshdila <maheshdilshan716@gmail.com>
2023-09-30 23:08:18 +0530

    "initialize"

---
+++ gringots/src/main/java/com/gringots/GringotsApplication.java
@@ -0,0 +1,13 @@
+package com.gringots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GringotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GringotsApplication.class, args);
+	}
+
+}
