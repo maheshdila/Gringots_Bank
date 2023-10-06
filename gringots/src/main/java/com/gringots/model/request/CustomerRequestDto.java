f65834d74c8ebeb5f17468b1b2fd7bae5edd5317
ThanuH <thanujahennayake@gmail.com>
2023-10-06 12:10:26 +0530

    Merge Issue Resolved In CustomerRequestDto

--- gringots/src/main/java/com/gringots/model/request/CustomerRequestDto.java
+++ gringots/src/main/java/com/gringots/model/request/CustomerRequestDto.java
@@ -21,20 +21,4 @@ public class CustomerRequestDto{
     private String organizationName;
     private String organizationRegNo;
     private String password;
-
-//    private String username;
-//    private String password;
-//    private String email;
-//    private String mobileNo;
-//    private String nic;
-//    private String passport;
-//    private String firstName;
-//    private String lastName;
-//    private String profilePicPath;
-//    private String latitude;
-//    private String longitude;
-//    private String nicImagePath;
-//    private String kycImagePath;
-//    private int referrerId;
-//    private String preferredLanguage;
 }
